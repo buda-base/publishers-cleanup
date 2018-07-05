47c9afb96e49f1d6a63c2a3ea8612cb0f7863f2c
drupchen <hhdrupchen@gmail.com>
2018-07-05 14:23:14 +0200

    update and improvements in csv format

--- input/parse-json.py
+++ input/parse-json.py
@@ -1,42 +1,59 @@
 import json
 import csv
 
+
+# Parses the json file, retaining three fields from the input:
+# - workPublisherLocation
+# - workPublisherName
+# - uri
+#
+# The columns of each .csv to be processed by OpenRefine is formatted as follows:
+#      location_orig, location_new, name_orig, name_new, url, uri
+# - (...)_orig columns are left untouched
+# - (...)_new columns are normalized in OpenRefine
+# - url leads to the work in the tbrc.org website. It is directly clickable from OpenRefine
+
+
 with open('publishers.json', 'r') as f:
     data = json.load(f)
 
+##################################################
+# separate the entries in the json in three csv
 TLM = []
 FPL = []
 results = []
 for result in data['results']['bindings']:
-    uri = result['w']['value']
+    url = 'https://www.tbrc.org/#!rid=' + result['w']['value'].split('/')[-1]
 
     location_value = result['workPublisherLocation']['value']
 
     name_value = result['workPublisherName']['value']
 
-    if 'TLM' in uri:
-        TLM.append((location_value, name_value, uri))
-    elif 'FPL' in uri:
-        FPL.append((location_value, name_value, uri))
+    if 'TLM' in url:
+        TLM.append((location_value, name_value, url))
+    elif 'FPL' in url:
+        FPL.append((location_value, name_value, url))
     else:
-        results.append((location_value, name_value, uri))
+        results.append((location_value, name_value, url))
 
-fieldnames = ['location_orig', 'location_new', 'name_orig', 'name_new', 'uri']
+##################################################
+# write the three csvs with the same headings
+fieldnames = ['location_orig', 'location_new', 'name_orig', 'name_new', 'url', 'uri']
 
 with open('../csv/publishers.csv', 'w') as g:
     writer = csv.writer(g, dialect='excel')
     writer.writerow(fieldnames)
-    for l_v, n_v, uri in results:
-        writer.writerow((l_v, l_v, n_v, n_v, uri))
+    for l_v, n_v, url in results:
+        writer.writerow((l_v, l_v, n_v, n_v, url))
 
 with open('../csv/TLM.csv', 'w') as h:
     writer = csv.writer(h, dialect='excel')
     writer.writerow(fieldnames)
-    for l_v, n_v, uri in TLM:
-        writer.writerow((l_v, l_v, n_v, n_v, uri))
+    for l_v, n_v, url in TLM:
+        writer.writerow((l_v, l_v, n_v, n_v, url))
 
 with open('../csv/FPL.csv', 'w') as i:
     writer = csv.writer(i, dialect='excel')
     writer.writerow(fieldnames)
-    for l_v, n_v, uri in FPL:
-        writer.writerow((l_v, l_v, n_v, n_v, uri))
+    for l_v, n_v, url in FPL:
+        writer.writerow((l_v, l_v, n_v, n_v, url))
--- log-modifications.py
+++ log-modifications.py
@@ -2,9 +2,22 @@ import csv
 import os
 from collections import defaultdict
 
+#################################################
+# delete existing log files in order to have a clean dir for generating the logs
+abs_log_paths = ['logs/locations/'+filename for filename in os.listdir('logs/locations')]
+abs_log_paths.extend(['logs/publishers/'+filename for filename in os.listdir('logs/publishers')])
+
+for log in abs_log_paths:
+    if os.path.isfile(log):
+        os.remove(log)
+
 modifs_loc = defaultdict(list)
 modifs_name = defaultdict(list)
 
+##################################################
+# find all the spelling variants in the csv files
+# a spelling variant is found when the (...)_orig and the (...)_new are not equal.
+# this shows the field was normalized using OpenRefine
 in_path = 'csv'
 for f in os.listdir(in_path):
     with open('{}/{}'.format(in_path, f)) as g:
@@ -13,11 +26,18 @@ for f in os.listdir(in_path):
             loc_orig, loc_new, name_orig, name_new, _ = row
 
             if loc_new != loc_orig and ((loc_new not in modifs_loc) or (loc_orig not in modifs_loc[loc_new])):
+                if '\t'+loc_new not in modifs_loc[loc_new]:
+                    modifs_loc[loc_new].append('\t'+loc_new)
                 modifs_loc[loc_new].append(loc_orig)
 
             if name_new != name_orig and ((name_new not in modifs_name) or (name_orig not in modifs_name[name_new])):
+                if '\t'+name_new not in modifs_name[name_new]:
+                    modifs_name[name_new].append('\t'+name_new)
                 modifs_name[loc_new].append(loc_orig)
 
+####################################################
+# each normalized entity gets a file with itself preceded by a tab as the first line,
+# then a list of each spelling variant per line
 for k, v in modifs_loc.items():
     with open('logs/locations/{}.txt'.format(k), 'w') as h:
         h.write('\n'.join(sorted(v)))
