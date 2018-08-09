d00141cb3f83f63b22a1d5906c9920f4190e31da
drupchen <hhdrupchen@gmail.com>
2018-08-09 19:40:34 +0100

    cleanup script

--- log-modifications.py
+++ log-modifications.py
@@ -2,37 +2,44 @@ import csv
 import os
 from collections import defaultdict
 
-#################################################
-# delete existing log files in order to have a clean dir for generating the logs
-abs_log_paths = ['logs/locations/'+filename for filename in os.listdir('logs/locations')]
-abs_log_paths.extend(['logs/publishers/'+filename for filename in os.listdir('logs/publishers')])
 
-for log in abs_log_paths:
-    if os.path.isfile(log):
-        os.remove(log)
+def delete_log_files():
+    """delete existing log files in order to have a clean dir for generating the logs"""
+    abs_log_paths = ['logs/locations/'+filename for filename in os.listdir('logs/locations')]
+    abs_log_paths.extend(['logs/publishers/'+filename for filename in os.listdir('logs/publishers')])
 
-modifs_loc = defaultdict(list)
-unknown_loc = defaultdict(list)
-modifs_name = defaultdict(list)
+    for log in abs_log_paths:
+        if os.path.isfile(log):
+            os.remove(log)
 
-##################################################
-# find all the spelling variants in the csv files
-# a spelling variant is found when the (...)_orig and the (...)_new are not equal.
-# this shows the field was normalized using OpenRefine
-with open('csv/publishers.csv') as g:
-    data = csv.reader(g)
-    rows = list(data)[1:]
-    sorted_rows = sorted(rows, key=lambda x: x[1])
 
-    for num, row in enumerate(sorted_rows):
+def process_refined_csv():
+    modifs_loc = defaultdict(list)
+    unknown_loc = defaultdict(list)
+    modifs_name = defaultdict(list)
+
+    rows = []
+    with open('csv/publishers.csv') as g:
+        data = csv.reader(g)
+        rows = list(data)[1:]
+        rows = sorted(rows, key=lambda x: x[1])  # sort on new_loc
+
+    for num, row in enumerate(rows):
         loc_orig, loc_new, name_orig, name_new, _ = row
 
-        if loc_new[0].isupper() and ((loc_new not in modifs_loc) or (loc_orig not in modifs_loc[loc_new])):
+        # I am certain of the modified location (capitals) and it needs to be added
+        if loc_new[0].isupper() \
+           and ((loc_new not in modifs_loc)
+                or (loc_orig not in modifs_loc[loc_new])):
+
+            # adding the header if it is not there
             if '\t'+loc_new not in modifs_loc[loc_new]:
                 modifs_loc[loc_new].append('\t'+loc_new)
+
+            # add current variant
             modifs_loc[loc_new].append(loc_orig)
 
-        #
+        # all entries not already processed
         elif loc_new not in modifs_loc.keys():
             unknown_loc[loc_new].append(loc_orig)
 
@@ -41,31 +48,37 @@ with open('csv/publishers.csv') as g:
         #         modifs_name[name_new].append('\t'+name_new)
         #     modifs_name[loc_new].append(loc_orig)
 
-####################################################
-# each normalized entity gets a file with itself preceded by a tab as the first line,
-# then a list of each spelling variant per line
-for k, v in modifs_loc.items():
-    with open('logs/locations/{}.txt'.format(k), 'w') as h:
-        h.write('\n'.join(sorted(v)))
-
-for k, v in modifs_name.items():
-    with open('logs/publishers/{}.txt'.format(k), 'w') as h:
-        h.write('\n'.join(sorted(v)))
+    return modifs_loc, unknown_loc, modifs_name
 
 
-chars_to_strip = '[]［］{}/／/?'
-with open('logs/locations.tsv', 'w') as f:
-    f.write('PublisherLocations\t\t\t\n')
-    for name, variants in modifs_loc.items():
-        f.write('\t{}\t\t\n'.format(name.strip(chars_to_strip).strip('\t').strip()))
-        variants = [v.strip(chars_to_strip).strip('\t').strip() for v in variants]
-        for v in sorted(list(set(variants))):
-            f.write('\t\t\t{}\n'.format(v.strip()))
+def write_individual_entries(type, entry):
+    for k, v in entry.items():
+        with open('logs/{}/{}.txt'.format(type, k), 'w') as h:
+            v = sorted(v)
+            h.write('\n'.join(v))
 
-with open('logs/locations_unknown.tsv', 'w') as f:
-    f.write('PublisherLocations\t\t\t\n')
-    for name, variants in unknown_loc.items():
-        f.write('\t{}\t\t\n'.format(name.strip(chars_to_strip).strip('\t').strip()))
-        variants = [v.strip(chars_to_strip).strip('\t').strip() for v in variants]
-        for v in sorted(list(set(variants))):
-            f.write('\t\t\t{}\n'.format(v.strip()))
+
+def write_full_log(type, entries):
+    chars_to_strip = '[]［］{}/／/?'
+    with open('logs/{}.tsv'.format(type), 'w') as f:
+        title = 'PublisherLocations\t\t\t\n'
+        f.write(title)
+
+        for name, variants in entries.items():
+            entry = '\t{}\t\t\n'.format(name.strip(chars_to_strip).strip('\t').strip())
+            f.write(entry)
+
+            cleaned_variants = [v.strip(chars_to_strip).strip('\t').strip() for v in variants]
+            sorted_variants = sorted(list(set(cleaned_variants)))
+            for v in sorted_variants:
+                v = v.strip()
+                f.write('\t\t\t{}\n'.format(v))
+
+
+if __name__ == '__main__':
+    delete_log_files()
+    modified_locs, unknown_locs, modified_names = process_refined_csv()
+    write_individual_entries('locations', modified_locs)
+    write_individual_entries('publishers', modified_names)
+    write_full_log('locations', modified_locs)
+    write_full_log('locations_unknown', unknown_locs)
