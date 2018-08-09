52e28a7d751d47be9a40e3b621adf0b4cafe8602
drupchen <hhdrupchen@gmail.com>
2018-08-09 19:09:57 +0100

    update and list of all unknown locations

---
+++ README.md
@@ -0,0 +1,11 @@
+workflow:
+
+import in MindMeister as tab-indented .txt
+export as Freemind file
+modify in Freemind
+present/modify in MindMeister
+Export as txt
+
+
+Todo: check that there are no double entries checking from the RIDs
+
--- log-modifications.py
+++ log-modifications.py
@@ -12,31 +12,34 @@ for log in abs_log_paths:
         os.remove(log)
 
 modifs_loc = defaultdict(list)
+unknown_loc = defaultdict(list)
 modifs_name = defaultdict(list)
 
 ##################################################
 # find all the spelling variants in the csv files
 # a spelling variant is found when the (...)_orig and the (...)_new are not equal.
 # this shows the field was normalized using OpenRefine
-in_path = 'csv'
-for f in os.listdir(in_path):
-    with open('{}/{}'.format(in_path, f)) as g:
-        data = csv.reader(g)
-        for num, row in enumerate(list(data)[1:]):
-            loc_orig, loc_new, name_orig, name_new, _ = row
+with open('csv/publishers.csv') as g:
+    data = csv.reader(g)
+    rows = list(data)[1:]
+    sorted_rows = sorted(rows, key=lambda x: x[1])
 
-            if loc_new[0].isupper() and loc_new != loc_orig and ((loc_new not in modifs_loc) or (loc_orig not in modifs_loc[loc_new])):
-                if '\t'+loc_new not in modifs_loc[loc_new]:
-                    modifs_loc[loc_new].append('\t'+loc_new)
-                modifs_loc[loc_new].append(loc_orig)
+    for num, row in enumerate(sorted_rows):
+        loc_orig, loc_new, name_orig, name_new, _ = row
 
-            #
-            else:
-                pass
-            # if name_new != name_orig and ((name_new not in modifs_name) or (name_orig not in modifs_name[name_new])):
-            #     if '\t'+name_new not in modifs_name[name_new]:
-            #         modifs_name[name_new].append('\t'+name_new)
-            #     modifs_name[loc_new].append(loc_orig)
+        if loc_new[0].isupper() and ((loc_new not in modifs_loc) or (loc_orig not in modifs_loc[loc_new])):
+            if '\t'+loc_new not in modifs_loc[loc_new]:
+                modifs_loc[loc_new].append('\t'+loc_new)
+            modifs_loc[loc_new].append(loc_orig)
+
+        #
+        elif loc_new not in modifs_loc.keys():
+            unknown_loc[loc_new].append(loc_orig)
+
+        # if name_new != name_orig and ((name_new not in modifs_name) or (name_orig not in modifs_name[name_new])):
+        #     if '\t'+name_new not in modifs_name[name_new]:
+        #         modifs_name[name_new].append('\t'+name_new)
+        #     modifs_name[loc_new].append(loc_orig)
 
 ####################################################
 # each normalized entity gets a file with itself preceded by a tab as the first line,
@@ -54,6 +57,14 @@ chars_to_strip = '[]［］{}/／/?'
 with open('logs/locations.tsv', 'w') as f:
     f.write('PublisherLocations\t\t\t\n')
     for name, variants in modifs_loc.items():
+        f.write('\t{}\t\t\n'.format(name.strip(chars_to_strip).strip('\t').strip()))
+        variants = [v.strip(chars_to_strip).strip('\t').strip() for v in variants]
+        for v in sorted(list(set(variants))):
+            f.write('\t\t\t{}\n'.format(v.strip()))
+
+with open('logs/locations_unknown.tsv', 'w') as f:
+    f.write('PublisherLocations\t\t\t\n')
+    for name, variants in unknown_loc.items():
         f.write('\t{}\t\t\n'.format(name.strip(chars_to_strip).strip('\t').strip()))
         variants = [v.strip(chars_to_strip).strip('\t').strip() for v in variants]
         for v in sorted(list(set(variants))):
