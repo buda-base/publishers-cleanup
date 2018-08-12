a261b2c8ec96fc2f734f7a4809f31c00a4f35f80
drupchen <hhdrupchen@gmail.com>
2018-08-12 23:28:02 +0100

    enable phonetic matching using bophono and THL's wylie2unicode lib
    (there seem to be an infinite loop somewhere, so no phonetic matches found yet)

--- match_existing_place_RID.py
+++ match_existing_place_RID.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import csv
 from collections import defaultdict
+import bophono
+import unicodedata
+
+from third_party.convert import wylie2unicode
 
 
 def clean_entry(entry):
@@ -43,13 +47,67 @@ def parse_locations():
     return locations
 
 
-def exact_match_places(rids, locations):
+def is_exact_match(text1, text2):
+    return text1 == text2
+
+
+unicodes = {}
+phonos = {}
+
+
+def is_phonetic_match(text1, text2):
+    global unicodes, phonos
+    # attempt to detect non-wylie
+    if text1 == ''\
+            or text2 == ''\
+            or 'CJK' in unicodedata.name(text1[0])\
+            or 'CJK' in unicodedata.name(text2[0])\
+            or text1[0].isupper()\
+            or text2[0].isupper():
+        return False
+
+    else:
+        options = {
+            'aspirateLowTones': True,
+            'prefixStrategy': 'always'
+        }
+        converter = bophono.UnicodeToApi(options=options)
+        dir = str(Path.cwd() / 'third_party' / 'Lingua-BO-Wylie')
+        if text1 not in unicodes.keys():
+            uni1 = wylie2unicode(text1, dir)
+            unicodes[text1] = uni1
+        else:
+            uni1 = unicodes[text1]
+
+        if text2 not in unicodes.keys():
+            uni2 = wylie2unicode(text2, dir)
+            unicodes[text2] = uni2
+        else:
+            uni2 = unicodes[text2]
+
+        if uni1 not in phonos.keys():
+            phono1 = converter.get_api(uni1)
+            phonos[uni1] = phono1
+        else:
+            phono1 = phonos[uni1]
+
+        if uni2 not in phonos.keys():
+            phono2 = converter.get_api(uni2)
+            phonos[uni2] = phono2
+        else:
+            phono2 = phonos[uni2]
+        if phono1 == phono2:
+            print('\n', text1, text2, '\n')
+        return phono1 == phono2
+
+
+def match(rids, locations, match_func):
     matches = {}
     for loc, l_variants in locations.items():
         for rid, r_variants in rids.items():
             for l in l_variants:
                 for r in r_variants:
-                    if l == r:
+                    if match_func(l, r):
                         matches[loc] = rid
     return matches
 
@@ -57,6 +115,11 @@ def exact_match_places(rids, locations):
 if __name__ == '__main__':
     parsed = parse_existing_places()
     locations = parse_locations()
-    matches = exact_match_places(parsed, locations)
+
+    matches = match(parsed, locations, is_exact_match)
     out = '\n'.join(['{},{}'.format(k, v) for k, v in matches.items()])
     Path('exact_matches.csv').write_text(out)
+
+    phono_matches = match(parsed, locations, is_phonetic_match)
+    out = '\n'.join(['{},{}'.format(k, v) for k, v in phono_matches.items()])
+    Path('phono_matches.csv').write_text(out)
